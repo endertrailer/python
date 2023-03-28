296cb97aca2e3d37ec52768f83b77dd7b8c44326
endertrailer <121684728+endertrailer@users.noreply.github.com>
2023-03-28 14:51:19 +0530

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -2,17 +2,18 @@ answer_file = open("hello.txt", "a")
 num1 = float(input("enter a number: "))
 num2 = float(input("enter second number: "))
 operator = input("enter operation: ")
+write = answer_file.write
 if operator == "+":
-    answer_file.write(", ")
-    answer_file.write(str(num1 + num2))
+    write(", ")
+    write(str(num1 + num2))
 elif operator == "*":
-    answer_file.write(", ")
-    answer_file.write(str(num1 * num2))
+    write(", ")
+    write(str(num1 * num2))
 elif operator == "-":
-    answer_file.write(", ")
-    answer_file.write(str(num1 - num2))
+    write(", ")
+    write(str(num1 - num2))
 elif operator == "/":
-    answer_file.write(", ")
-    answer_file.write(str(float(num1 / num2)))
+    write(", ")
+    write(str((num1 / num2)))
 else:
     print("enter a valid operator: ")
